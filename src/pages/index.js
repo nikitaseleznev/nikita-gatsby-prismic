946a14be5a3b5f8eab11885dae6d6d71a1bf4585
nikitaseleznev <43063687+nikitaseleznev@users.noreply.github.com>
2018-12-31 14:09:59 +0300

    Revert "nik gp"
    This reverts commit d1449516298c9ef87c963733d67088b81f699bc9.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,28 +6,9 @@ import Layout from '../components/layout'
 const IndexPage = () => (
   <Layout>
     <h1>NIKITA SELEZNEV</h1>
-    <div className="img">
-    {data.img && data.img && (
-      <Img fluid={data.img.childImageSharp.fluid} />
-    )}
-    {console.log(data)}
-    </div>
+    <p style={{paddingLeft: '12px'}}><Link to="/projects/">PROJECTS</Link></p>
+    <p style={{paddingLeft: '12px'}}><Link to="/about/">ABOUT+CONTACTS</Link></p>
   </Layout>
 )
 
 export default IndexPage
-export const pageQuery = graphql`
-  query AboutQuery {
-    img: file(relativePath: {eq: "91.jpg"}) {
-      childImageSharp {
-        fluid(maxWidth: 720, quality: 80, traceSVG: {
-          color: "#000000"
-          turnPolicy: TURNPOLICY_MINORITY
-          blackOnWhite: false
-        }) {
-          ...GatsbyImageSharpFluid_tracedSVG
-        }
-      }
-    }
-  }
-`
