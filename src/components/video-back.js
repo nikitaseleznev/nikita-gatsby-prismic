0313a87303135d40db0a68a5855d40bc205a8403
Alexei Accio <alexei.accio@gmail.com>
2019-02-04 21:05:09 +0300

    Update video-back.js

--- src/components/video-back.js
+++ src/components/video-back.js
@@ -1,41 +1,48 @@
-import React, { memo, useState } from 'react';
+import React, { PureComponent } from 'react';
 import YouTube from 'react-youtube';
 
-const Video = () => {
-  const opts = {
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 1,
-      disablekb: 1,
-      controls: 0,
-      loop: 1,
-      version: 3,
-      showinfo: 0,
-      rel: 0,
-    },
-  };
+class Video extends PureComponent {
+  constructor() {
+    super();
+    this.state = {
+      isPlay: false,
+    };
+  }
 
-  const [isPlay, setPlay] = useState(false);
-
-  const onReady = event => {
+  onReady = event => {
     event.target.mute();
     event.target.playVideo();
-    setPlay(true);
+    this.setState({ isPlay: true });
   };
 
-  const onEnd = event => {
+  onEnd = event => {
     event.target.playVideo();
   };
 
-  return isPlay ? (
-    <YouTube
-      videoId="JCrmFHC9sSM"
-      className="video-iframe"
-      opts={opts}
-      onReady={onReady}
-      onEnd={onEnd}
-    />
-  ) : null;
-};
+  render() {
+    const opts = {
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+        disablekb: 1,
+        controls: 0,
+        loop: 1,
+        version: 3,
+        showinfo: 0,
+        rel: 0,
+      },
+    };
 
-export default memo(Video);
+    return isPlay ? (
+      <YouTube
+        videoId="JCrmFHC9sSM"
+        className="video-iframe"
+        opts={opts}
+        onReady={this.onReady}
+        onEnd={this.onEnd}
+      />
+    ) : null;
+  }
+}
+
+export default Video;
