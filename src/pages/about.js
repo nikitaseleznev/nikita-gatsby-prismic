14db9ebf678297f809326e57f01a02eb36515183
nikitaseleznev <43063687+nikitaseleznev@users.noreply.github.com>
2022-02-20 18:53:42 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,10 @@ const IndexPage = ({ data, location }) => (
   <Layout location={location}>
     <h2>About</h2>
     <p>Nikita Seleznev (b. Perm, 1990) was trained at the Saint Petersburg Stieglitz Academy of Art and Design. His concentration was in monumental-decorative sculpture, a distinctive curriculum best known in Russia and the countries of the former Soviet Bloc. His aesthetic awareness was informed primarily by European Modernism, particularly cubism, surrealism, as well as the more recent polystylism of the 1990’s and early 2000’s. 
-
-Seleznev challenges the place and logic of sculpture in a modern, more international context. Interested in materiality, Seleznev has worked with more conventional sculptural materials—for example, clay and concrete—as well as found objects, textiles, metal, plastics and composite materials. He often combines objects, both handmade and computer-assisted, with sound and video in his sculptural installations, paying attention to three sensory perceptions except: touch, sight and touch.
- 
-In one of his recent projects, Karate Poetry (St. Petersburg, 2020), Seleznev visited a variety of online communities, where fresh visual culture is always being formed. He specifically questioned how sculpture might transformed from an idea (e.g., language or an expression) in a physical object, which could then be retransformed to then into a digital one.
-
+</p>
+<p>Seleznev challenges the place and logic of sculpture in a modern, more international context. Interested in materiality, Seleznev has worked with more conventional sculptural materials—for example, clay and concrete—as well as found objects, textiles, metal, plastics and composite materials. He often combines objects, both handmade and computer-assisted, with sound and video in his sculptural installations, paying attention to three sensory perceptions except: touch, sight and touch.
+ </p>
+<p>In one of his recent projects, Karate Poetry (St. Petersburg, 2020), Seleznev visited a variety of online communities, where fresh visual culture is always being formed. He specifically questioned how sculpture might transformed from an idea (e.g., language or an expression) in a physical object, which could then be retransformed to then into a digital one.
    </p>
 
     <p>
