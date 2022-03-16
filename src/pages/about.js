48f755b6ee0b202248371be5d422e675a6aab079
nikitaseleznev <43063687+nikitaseleznev@users.noreply.github.com>
2022-03-16 22:14:25 +0300

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,16 +4,18 @@ import Layout from '../components/layout'
 const IndexPage = ({ data, location }) => (
   <Layout location={location}>
     <h2>About</h2>
-    <p>Nikita Seleznev
-St. Petersburg, Russia
+    <p>Nikita Seleznev	born 1990, Perm—lives Tbilisi, Georgia (temporary)
 </p>
-<p>Nikita Seleznev is a multi-media artist, who lives and works in St. Petersburg. Born in Perm in 1990, he studied sculpture at the St. Petersburg Stieglitz State Academy of Art and Design. 
+<p>Nikita Seleznev is a multi-media artist who studied sculpture at the St. Petersburg Stieglitz State Academy of Art and Design.
+ </p>
+<p>Since 2018, Seleznev has had several solo exhibitions, including My Green Crocodile.Navicula Artis, St. Petersburg (2018); Karate Poetry, MYTH, St. Petersburg (2020) and Suburbia, Victoria Art Gallery, Samara (2020), and most recently Unfinished Proiect, The Boris Yeltsin Presidential Center, Ekaterinburg, (2021).
 </p>
-<p>Since 2018, Seleznev has had several solo exhibitions, including My Green Crocodile, Navicula Artis, St. Petersburg (2018); Karate Poetry, MYTH, St. Petersburg (2020) and Suburbia, Victoria Art Gallery, Samara (2020) and Unfinished Project, The Boris Yeltsin Presidential Center, Ekaterinburg, (2021).
- His work has been featured in several group exhibitions, including Los Atravesados, Treize, Paris (2018); Horizon-19, New Museum of Modern Art, St. Petersburg (2019); I don't know whether the Earth is spinning or not…, VII Moscow International Biennale of Young Art (2020); 20:20. Time Has Stopped, Moscow Museum of Modern Art (2020); Assuming Distance: Speculations, Fakes, and Predictions in the Age of the Coronacene, Garage Museum of Contemporary Art, Moscow (2021) and Thinking hands touching each other, the main project of the 6th Ural Industrial Biennial, Ekaterinburg (2021) .  
+<p>His work has been featured in several group exhibitions, including  Los Atravesados, Treize gallery, Paris (2018); Horizon-19, New Museum of Modern Art, St. Petersburg (2019); I don't know whether the Earth is spinning or not..., VIl Moscow International Biennale of Young Art (2020); 20:20. Time Has Stopped, Moscow Museum of Modern Art (2020); Assuming Distance: Speculations, Fakes, and Predictions in the Age of the Coronacene, Garage Museum of
+Contemporary Art, Moscow (2021), and Thinking hands touching each other, 6th Ural Industrial Biennial, Ekaterinburg (2021) .
 </p>
-<p>Karate Poetry was shortlisted for the "Innovation-2021" State Prize in the "New Generation" category. Previously, Seleznev participated in the CEC ArtsLink Art Prospect Fellows Program (New York, 2019) and was a resident of the Garage Studios (Moscow, 2021).
-</p>
+<p>Karate Poetry was shortlisted for the "Innovation-2021" State Prize in the "New Generation" category. Previously, Seleznev participated in the CC ArtsLink Art Prospect Fellows Program (New York, 2019) and was a resident of the Garage Studios
+(Moscow, 2021)
+ </p>
 
     <p>
       <a href="https://docs.google.com/document/d/e/2PACX-1vRioH2RT1qibRX1Wl98U-nf-3srSwI84rYBpHNDq35nQdDKp7bOZHCnVU5K-8V_fV_B30EHXHBQEk58/pub?embedded=true" target="_blank" rel="noopener noreferrer">CV</a>&nbsp;&#47;&#47; seleznevnikitasculptura@gmail.com
