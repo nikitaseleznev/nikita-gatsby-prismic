2641e52332027ad9eb5455331cd0c131c0f00600
manhir <53478716+manhir@users.noreply.github.com>
2022-03-25 16:15:52 +0300

    upd /index

--- src/components/video-back.js
+++ src/components/video-back.js
@@ -1,53 +1,57 @@
 import React, { PureComponent } from 'react';
 import YouTube from 'react-youtube';
+import s from './video-back.module.css'
 
 class Video extends PureComponent {
-  constructor() {
-    super();
-    this.state = {
-      isPlay: false,
-    };
-  }
+    constructor() {
+        super();
+        this.state = {
+            isPlay: false,
+        };
+    }
 
-  onReady = event => {
-    event.target.mute();
-    event.target.playVideo();
-    this.setState({ isPlay: true });
-  };
-
-  onEnd = event => {
-    event.target.playVideo();
-  };
-
-  render() {
-    const opts = {
-      playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1,
-        disablekb: 1,
-        controls: 0,
-        loop: 1,
-        version: 3,
-        showinfo: 0,
-        rel: 0,
-      },
+    onReady = event => {
+        event.target.mute();
+        event.target.playVideo();
+        this.setState({ isPlay: true });
     };
 
-    return (
-      <div
-        className="video-wrapper"
-        style={{ opacity: this.state.isPlay ? 1 : 0 }}
-      >
-        <YouTube
-          videoId="u9MoR4xDOv4"
-          className="video-iframe"
-          opts={opts}
-          onReady={this.onReady}
-          onEnd={this.onEnd}
-        />
-      </div>
-    );
-  }
+    onEnd = event => {
+        event.target.playVideo();
+    };
+
+    render() {
+        const opts = {
+            playerVars: {
+                // https://developers.google.com/youtube/player_parameters
+                autoplay: 1,
+                disablekb: 1,
+                controls: 0,
+                loop: 1,
+                version: 3,
+                showinfo: 0,
+                rel: 0,
+            },
+        };
+
+        return (
+            <div className={s.videoBackground}>
+                <div className={s.videoForeground}>
+                    <div
+                        className={s.videoWrapper}
+                        style={{ opacity: this.state.isPlay ? 1 : 0 }}
+                    >
+                        <YouTube
+                            videoId="u9MoR4xDOv4"
+                            opts={opts}
+                            onReady={this.onReady}
+                            onEnd={this.onEnd}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Video;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Video from '../components/video-back';
 
 import Layout from '../components/layout'
 
 const IndexPage = ({ location }) => (
-  <Layout location={location}>
-    <h1>NIKITA SELEZNEV</h1>
-  </Layout>
+    <Layout location={location}>
+        <Video />
+    </Layout>
 )
 
 export default IndexPage
