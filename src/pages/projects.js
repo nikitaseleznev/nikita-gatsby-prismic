f7e07b938ffacc98e435b288c3eb5ffe08a1f60b
manhir <53478716+manhir@users.noreply.github.com>
2022-03-25 17:45:55 +0300

    cleaning

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,15 +2,15 @@ import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-export default ({data, location}) => (
- <Layout location={location}>
-   <h1>Projects</h1>
-   <ul>
-      {data.projects.edges.map(({ node }) => (
-        <li key={node.uid}><Link to={node.uid}>{node.data.title.text}</Link></li>
-      ))}
-    </ul> 
- </Layout>
+export default ({ data, location }) => (
+    <Layout location={location}>
+        <h1>Projects</h1>
+        <ul>
+            {data.projects.edges.map(({ node }) => (
+                <li key={node.uid}><Link to={node.uid}>{node.data.title.text}</Link></li>
+            ))}
+        </ul>
+    </Layout>
 )
 
 export const pageQuery = graphql`
